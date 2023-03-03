23954f42f43916e6221381882523d17a369b646c
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-03-03 19:52:19 +0700

    api for findAllCourses from db-data json

--- nestjs-course-1-start/rest-api/src/courses/controllers/courses.controller.ts
+++ nestjs-course-1-start/rest-api/src/courses/controllers/courses.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Get } from "@nestjs/common";
+import { findAllCourses } from "../../../db-data";
+import { Course } from '../../../../shared/course';
+
 
 @Controller()
  export class CourseController {
 
-  @Get('/api/hello-world')
-  async helloWorld(): Promise<string> {
-    return 'Hello World';
+  // @Get('/api/hello-world')
+  // async helloWorld(): Promise<string> {
+  //   return 'Hello World';
+
+  // }
+
+  @Get('/api/courses')
+  async findAllCourses(): Promise <Course[] | any> {
+
+    return findAllCourses();
+
+    //return 'test';
+
 
   }
 
